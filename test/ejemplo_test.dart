490321ff10821a4d529aa56aa4a39c9d6db2bddd
unknown <moinavajas97@gmail.com>
2025-05-27 19:05:46 -0400

    prueba unitaria básica 3 CI

---
+++ test/ejemplo_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('El número 2 más 2 debe ser igual a 4', () {
+    const resultado = 2 + 2;
+    expect(resultado, 4);
+  });
+}
